98b1b4c7e1edf733484526a880d7738a3695a5b8
uriber <berman.uri@gmail.com>
2023-04-29 18:34:44 +0300

    added code for get api

--- api-lesson/src/App.js
+++ api-lesson/src/App.js
@@ -1,9 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
 
 function App() {
   const chilki = 5
-  
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+      fetch('https://date.nager.at/api/v2/publicholidays/2023/US')
+        .then((response) => response.json())
+        .then((data) => {
+            console.log(data);
+            setPosts(data);
+        })
+        .catch((err) => {
+            console.log(err.message);
+        });
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
@@ -21,6 +35,16 @@ function App() {
         </a>
       </header>
       <p>hi this is my text {chilki}</p>
+      <div className="posts-container">
+      {posts.map((post) => {
+         return (
+            <div className="post-card" key={post.id}>
+               <h2 className="post-title">{post.localName}</h2>
+               <p className="post-body">{post.date}</p>
+            </div>
+         );
+      })}
+   </div>
     </div>
   );
 }
